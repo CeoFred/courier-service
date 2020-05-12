4a084608ef072612d232d6854e9e84c4e1e11e25
Johnson awah Alfred <32965534+CeoFred@users.noreply.github.com>
2020-05-12 23:00:04 +0100

    Update db.php

--- api/config/db.php
+++ api/config/db.php
@@ -2,16 +2,16 @@
 class Database{
 
     // heroku credentials
-//     private $host = "us-cdbr-east-06.cleardb.net";
-//     private $db_name = "heroku_cfa038a59a6920c";
-//     private $username = "b8e93990aa4e63";
-//     private $password = "8d33f7af ";
+    private $host = "us-cdbr-east-06.cleardb.net";
+    private $db_name = "heroku_cfa038a59a6920c";
+    private $username = "b8e93990aa4e63";
+    private $password = "8d33f7af ";
     
-$url = parse_url("mysql://b8e93990aa4e63:8d33f7af@us-cdbr-east-06.cleardb.net/heroku_cfa038a59a6920c?reconnect=true");
-private $host = $url["host"];
-private $username = $url["user"];
-private $password = $url["pass"];
-private $db_name = substr($url["path"], 1);
+// $url = parse_url("mysql://b8e93990aa4e63:8d33f7af@us-cdbr-east-06.cleardb.net/heroku_cfa038a59a6920c?reconnect=true");
+// private $host = $url["host"];
+// private $username = $url["user"];
+// private $password = $url["pass"];
+// private $db_name = substr($url["path"], 1);
     
     //local host crednetials
 //     private $host = "localhost";
